005740d7812f7922a30bdbd385c87f4150dfc4d1
Kien Nguyen <k.nguyen0125@gmail.com>
2019-12-29 10:49:01 +0700

    update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,6 @@ alias cls='clear' # Good 'ol Clear Screen command
 # Rebind vi/mim to neovim
 alias vi='nvim'
 alias vim='nvim'
+
+# Update Grub
+alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
