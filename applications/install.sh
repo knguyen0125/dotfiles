98cc1e65d8929e907304699a87d8df878e8571c3
Kien Nguyen <k.nguyen0125@gmail.com>
2020-05-27 10:28:55 +0700

    feat: symlink intellij idea to use nvm and conda

--- applications/install.sh
+++ applications/install.sh
@@ -1,4 +1,7 @@
 echo -en "Copying all desktop files to local applications..."
+# Create applications folder if not exists
 mkdir -p $HOME/.local/share/applications
-cp $ZSHDOT/applications/desktop/* $HOME/.local/share/applications
+
+# Symlink customized applications to desktop
+ln -sf $ZSHDOT/applications/desktop/* $HOME/.local/share/applications
 echo "Done"
