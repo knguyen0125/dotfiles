228781a93859fe04de41a5e53fc3f8f88d22f5ea
Kevin Nguyen <k.nguyen0125@gmail.com>
2020-05-28 23:38:30 +0700

    feat(hdmi-audio): add reload script for hdmi audio on suspend

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,3 +8,6 @@ alias vim='nvim'
 
 # Update Grub
 alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+
+# Reset HDMI Audio after suspend
+alias reset-hdmi-audio="/usr/bin/pasuspender /bin/true"
